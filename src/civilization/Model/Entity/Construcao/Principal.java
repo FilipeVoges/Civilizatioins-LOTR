fbb022a43850c1e0661fde1dc451100458cb115c
Filipe Voges <filipe.vogesh@gmail.com>
2017-06-08 10:56:42 -0300

    Criado as Entidades

---
+++ src/civilization/Model/Entity/Construcao/Principal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civilization.Model.Entity.Construcao;
+
+/**
+ *
+ * @author filipe
+ */
+public class Principal {
+    
+}
